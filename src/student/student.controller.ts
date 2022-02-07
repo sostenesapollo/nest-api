27b440b273fd3ec1be88d4e315fdb9180ff077c4
Sostenes Apollo <sostenesapollo25@gmail.com>
2022-02-07 20:06:58 -0300

    feat: logging body

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,9 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Put, Body } from '@nestjs/common';
 
 @Controller('students')
 export class StudentController {
     @Get()
     getStudents() {
         return "All Students"
+    }
+
+    @Get('/:id')
+    getStudentById(
+        @Param('id') params: { id: string }
+    ) {
+        console.log(params)
+        return params
+    }
+
+    @Post()
+    createStudent(
+        @Body() body
+    ) {
+        console.log(body)
+        return body
+    }
+
+    @Put('/:id')
+    updateStudent() {
+        return "Update Student"
     }
 }
