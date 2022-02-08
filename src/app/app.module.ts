e22117861afbb34d8043e1a97b492bbe7ea38b4b
Sostenes Apollo <sostenesapollo25@gmail.com>
2022-02-07 21:10:30 -0300

    feat: crud generated via command line

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { StudentController } from 'src/student/student.controller';
 import { TeacherController } from 'src/teacher/teacher.controller';
+import { TodosController } from 'src/todos/todos.controller';
+import { TodosService } from 'src/todos/todos.service';
 import { StudentService } from '../student/student.service';
 
 @Module({
   imports: [],
-  controllers: [StudentController, TeacherController],
-  providers: [StudentService]
+  controllers: [StudentController, TeacherController, TodosController],
+  providers: [StudentService, TodosService],
 })
 export class AppModule {}
